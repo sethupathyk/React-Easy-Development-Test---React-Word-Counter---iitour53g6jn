0ac8dbc21caa96c01c6ded5fbc07e9659876e235
Sethupathy <sethupathy.k.eng@gmail.com>
2021-12-29 19:50:08 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const wordWritten = (e) => {
+    // setCounter(e.target.value.length);
+    setCounter(e.target.value.split(" ").length);
+  };
+
+  
+    
 
 
   return (
-    <div id="main">
+    <div className="App">
+      <h2>Word limit input</h2>
+      <input type="text" placeholder="Enter Limit" />
+      <br /> <br />
+      <input
+        type="text"
+        placeholder="Enter the text"
+        onChange={wordWritten}/>
+      <h3>Total number of words written {counter}</h3>
+      <h3>Total number of character used</h3>
     </div>
-  )
+  );
 }
-
-
-export default App;
